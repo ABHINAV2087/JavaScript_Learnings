cd9db3403dedb911786b90ff7e304598469cc1d6
ABHINAV2087 <abhinav.tirole2021@vitbhopal.ac.in>
2023-10-11 00:38:01 +0530

    update

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,38 @@
+"use strict"; // used to treat all js code as newer version;
+
+/* Primitive Data Types   */
+  
+let name = "abhinav" // string datatype
+
+let age = 18 // number datatype
+
+let isLoggedIn = false // boolean datatype
+
+// null => standalone value 
+
+// undefined 
+
+// symbol
+
+
+
+/* Reference Data Types   */
+
+// object 
+  
+        /* let person = {
+                name: 'John',
+                age: 30,
+                isStudent: false
+            }; */
+
+// array 
+
+        /* let colors = ['red', 'green', 'blue']; */
+
+// function
+
+        /* function greet(name) {
+                    return `Hello, ${name}!`;
+            }
+        */
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,25 @@
+let score = "40"
+
+let valueInNumber = number(score) // to convert any string to number
+
+let num = 10;
+let str = String(num); // str will be the string "10"
+
+let str1 = "20";
+let num1 = Number(str1); // num will be the number 20
+
+
+let num2 = 10;
+let bool = Boolean(num2); // bool will be true
+
+let str2 = "";
+let bool2 = Boolean(str2); // bool2 will be false
+
+let num3 = 10;
+let str3= String(num3); // str will be the string "10"
+
+let now = new Date();
+let str4 = now.toString(); // str will be the date as a string
+
+let arr = [1, 2, 3];
+let str5 = arr.toString(); // str will be "1,2,3"
